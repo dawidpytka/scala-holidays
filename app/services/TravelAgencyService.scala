dfb8662fb90a3010b1321c44203bb6faa6552e00
dawid <dawid.pytka1@gmail.com>
2021-06-10 15:49:50 +0200

    fix error with no country in TUI

--- app/services/TravelAgencyService.scala
+++ app/services/TravelAgencyService.scala
@@ -14,18 +14,18 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 
-class TravelAgencyService @Inject() (){
+class TravelAgencyService @Inject()() {
   val format1 = new java.text.SimpleDateFormat("yyyy-MM-dd")
   val format2 = new java.text.SimpleDateFormat("dd-MM-yyyy")
   val format3 = new SimpleDateFormat("dd.MM.yyyy")
-  var countryCodesTUI: mutable.Map[String, String] = mutable.HashMap("ALBANIA"->"TIA", "ARUBA"->"AUA", "BARBADOS"->"BGI", "BUŁGARIA"->"BG",
-    "CHORWACJA"->"HR","CYPR"->"CY", "CZARNOGÓRA"->"TGD", "DJERBA"->"DJE", "DOMINIKANA"->"DO", "EGIPT"->"EG", "GRECJA"->"GR",
-    "HISZPANIA"->"ES", "INDONEZJA"->"ID", "JAMAJKA"->"MBJ", "KENIA"->"MBA", "KOSTARYKA"->"SJO", "KUBA"->"CU", "FNC"->"MADERA", "MALEDIWY"->"MLE",
-    "MALTA"->"MLA", "MAROKO"->"AGA", "MRU"->"MAURITIUS", "MEKSYK"->"CUN", "PANAMA"->"PTY", "PORTUGALIA"->"PT", "SESZELE"->"SZ",
-    "SRI LANKA"->"CMB","PORTUGALIA"->"PT", "STANY ZJEDNOCZONE"->"US", "TAJLANDIA"->"TH", "TUNEZJA"->"TN", "TURCJA"->"TR", "WŁOCHY"->"IT",
-    "WYSPY KANARYJSKIE"->"IC", "WYSPY ZIELONEGO PRZYLĄDKA"->"CV", "WŁOCHY"->"IT", "ZANZIBAR"->"ZNZ", "ZJEDNOCZONE EMIRATY ARABSKIE"->"ZEA")
+  var countryCodesTUI: mutable.Map[String, String] = mutable.HashMap("ALBANIA" -> "TIA", "ARUBA" -> "AUA", "BARBADOS" -> "BGI", "BUŁGARIA" -> "BG",
+    "CHORWACJA" -> "HR", "CYPR" -> "CY", "CZARNOGÓRA" -> "TGD", "DJERBA" -> "DJE", "DOMINIKANA" -> "DO", "EGIPT" -> "EG", "GRECJA" -> "GR",
+    "HISZPANIA" -> "ES", "INDONEZJA" -> "ID", "JAMAJKA" -> "MBJ", "KENIA" -> "MBA", "KOSTARYKA" -> "SJO", "KUBA" -> "CU", "FNC" -> "MADERA", "MALEDIWY" -> "MLE",
+    "MALTA" -> "MLA", "MAROKO" -> "AGA", "MRU" -> "MAURITIUS", "MEKSYK" -> "CUN", "PANAMA" -> "PTY", "PORTUGALIA" -> "PT", "SESZELE" -> "SZ",
+    "SRI LANKA" -> "CMB", "PORTUGALIA" -> "PT", "STANY ZJEDNOCZONE" -> "US", "TAJLANDIA" -> "TH", "TUNEZJA" -> "TN", "TURCJA" -> "TR", "WŁOCHY" -> "IT",
+    "WYSPY KANARYJSKIE" -> "IC", "WYSPY ZIELONEGO PRZYLĄDKA" -> "CV", "WŁOCHY" -> "IT", "ZANZIBAR" -> "ZNZ", "ZJEDNOCZONE EMIRATY ARABSKIE" -> "ZEA")
 
-  def getBestOffers(dateFrom: Date, dateTo: Date, countries: List[String], numberOfPersons: Int = 1, minDays: Int = 1, minHotelRate: Int = 3, onlyLastMinute: Boolean = false, onlyAllInclusive: Boolean = false): ListBuffer[Offer] ={
+  def getBestOffers(dateFrom: Date, dateTo: Date, countries: List[String], numberOfPersons: Int = 1, minDays: Int = 1, minHotelRate: Int = 3, onlyLastMinute: Boolean = false, onlyAllInclusive: Boolean = false): ListBuffer[Offer] = {
     var bestOffers = new ListBuffer[Offer]()
 
     bestOffers = ListBuffer.concat(
@@ -33,7 +33,7 @@ class TravelAgencyService @Inject() (){
       getItakaOffers(dateFrom, dateTo, countries, numberOfPersons, minDays, minHotelRate, onlyLastMinute, onlyAllInclusive),
       getTraveliadaOffers(dateFrom, dateTo, countries, numberOfPersons, minDays, minHotelRate, onlyLastMinute, onlyAllInclusive),
       getTUIOffers(dateFrom, dateTo, countries, numberOfPersons, minDays, minHotelRate, onlyLastMinute, onlyAllInclusive))
-    bestOffers = bestOffers.sortWith((o1,o2) => o1.price < o2.price)
+    bestOffers = bestOffers.sortWith((o1, o2) => o1.price < o2.price)
     bestOffers.take(20)
   }
 
@@ -43,18 +43,18 @@ class TravelAgencyService @Inject() (){
     body = body.dropRight(1)
     body = body + """]},"Sortowanie":{"CzyPoDacie":false,"CzyPoCenie":true,"CzyPoOcenach":false,"CzyPoPolecanych":false,"CzyDesc":false},"CzyCenaZaWszystkich":false,"CzyGrupowac":true,"DlugoscPobytuMin":""""
     body = body + minDays
-    body = body + """" ,"KategoriaHoteluMin":""""+ minHotelRate
+    body = body + """" ,"KategoriaHoteluMin":"""" + minHotelRate
     body = body + """","MiastaWyjazdu":[], "Panstwa":["""
     if (countries != null && countries.nonEmpty) {
-      countries.foreach(c => body = body + """""""  + changeExtraSigns(c.toLowerCase) + """",""")
+      countries.foreach(c => body = body + """"""" + changeExtraSigns(c.toLowerCase) + """",""")
     }
     body = body.dropRight(1)
-    if(onlyLastMinute) body = body + """],"Promocje": ["last-minute""""
-    if(onlyAllInclusive) body = body + """],"Wyzywienia": ["all-inclusive""""
+    if (onlyLastMinute) body = body + """],"Promocje": ["last-minute""""
+    if (onlyAllInclusive) body = body + """],"Wyzywienia": ["all-inclusive""""
     body = body + """],"TerminWyjazduMin":""""
-    if (dateFrom!=null) body = body + format1.format(dateFrom)
+    if (dateFrom != null) body = body + format1.format(dateFrom)
     body = body + """","TerminWyjazduMax":""""
-    if (dateTo!=null) body = body + format1.format(dateTo)
+    if (dateTo != null) body = body + format1.format(dateTo)
 
     body = body + """","TypyTransportu":["air"],"CzyPotwierdzoneTerminy":false,"PokazywaneLotniska":"SAME","Paginacja":{"Przeczytane":"0","IloscDoPobrania":"18"},"CzyImprezaWeekendowa":false}"""
 
@@ -66,22 +66,22 @@ class TravelAgencyService @Inject() (){
 
     if (result.body.isEmpty) return List()
     val json: JsValue = Json.parse(result.body)
-    val trips = json \"Bloczki"
+    val trips = json \ "Bloczki"
 
     implicit val offerRead: Reads[ReadOffer] = (
-        (__  \ "BazoweInformacje" \ "OfertaNazwa").read[String] ~
-        ((__ \ "Ceny")(0) \ "CenaZaOsobeAktualna").read[Double] ~
-        (__  \ "BazoweInformacje" \ "OfertaURL").read[String] ~
-        ((__ \ "Ceny")(0) \ "LiczbaDni").read[Int] ~
+      (__ \ "BazoweInformacje" \ "OfertaNazwa").read[String] ~
+        ((__ \ "Ceny") (0) \ "CenaZaOsobeAktualna").read[Double] ~
+        (__ \ "BazoweInformacje" \ "OfertaURL").read[String] ~
+        ((__ \ "Ceny") (0) \ "LiczbaDni").read[Int] ~
         (__ \ "BazoweInformacje" \ "GwiazdkiHotelu").read[Double] ~
         (__ \ "Ocena" \ "Ocena").readWithDefault(0.0) ~
-        ((__ \ "Wyzywienia")(0) \ "Nazwa").read[String]
-      )(ReadOffer)
+        ((__ \ "Wyzywienia") (0) \ "Nazwa").read[String]
+      ) (ReadOffer)
 
     implicit val offersRead: Reads[List[ReadOffer]] = Reads.list(offerRead)
     val offers = trips.get.validate[List[ReadOffer]](offersRead)
 
-    val finalOffers = for (offer <- offers.get) yield Offer (
+    val finalOffers = for (offer <- offers.get) yield Offer(
       no = 0,
       name = offer.name,
       travelAgencyName = "Rainbow",
@@ -96,11 +96,11 @@ class TravelAgencyService @Inject() (){
   }
 
   private def getItakaOffers(dateFrom: Date, dateTo: Date, countries: List[String], numberOfPersons: Int, minDays: Int, minHotelRate: Int, onlyLastMinute: Boolean, onlyAllInclusive: Boolean): List[Offer] = {
-    var sourceUrl: String = "https://www.itaka.pl/wyniki-wyszukiwania/wakacje/?view=offerList"+"&adults="+numberOfPersons
-    if (dateFrom!=null) sourceUrl = sourceUrl + "&date-from=" + format1.format(dateFrom)
-    if (dateTo!=null) sourceUrl = sourceUrl + "&date-to=" + format1.format(dateTo)
+    var sourceUrl: String = "https://www.itaka.pl/wyniki-wyszukiwania/wakacje/?view=offerList" + "&adults=" + numberOfPersons
+    if (dateFrom != null) sourceUrl = sourceUrl + "&date-from=" + format1.format(dateFrom)
+    if (dateTo != null) sourceUrl = sourceUrl + "&date-to=" + format1.format(dateTo)
 
-    if(onlyAllInclusive) sourceUrl = sourceUrl + "&food=allInclusive"
+    if (onlyAllInclusive) sourceUrl = sourceUrl + "&food=allInclusive"
     if (countries != null && countries.nonEmpty) {
       sourceUrl = sourceUrl + "&dest-region="
       countries.foreach(c => sourceUrl = sourceUrl + changeExtraSigns(c.toLowerCase) + "%2C")
@@ -116,18 +116,18 @@ class TravelAgencyService @Inject() (){
       case 6 | 7 | 8 => sourceUrl = sourceUrl + "&duration=from6to9"
       case 9 | 10 | 12 => sourceUrl = sourceUrl + "&duration=from9to12"
     }
-    if(minDays > 12) sourceUrl = sourceUrl + "&duration=from13"
+    if (minDays > 12) sourceUrl = sourceUrl + "&duration=from13"
 
     val htmlDocument = Jsoup.connect(sourceUrl).get()
     if (!htmlDocument.select(".empty-results").isEmpty) return List()
     val offersDomElements = htmlDocument.select(".offer").not(".promoOffer").asScala
-    val offersData = for(offerElement <- offersDomElements)
-      yield Offer (
+    val offersData = for (offerElement <- offersDomElements)
+      yield Offer(
         no = 1,
         name = offerElement.select(".header_title").text(),
         travelAgencyName = "Itaka",
-        price = offerElement.select(".current-price_value").html().substring(0,offerElement.select(".current-price_value").html().indexOf("&nbsp")).replaceAll("\\s", "").toDouble,
-        link = "https://www.itaka.pl"+offerElement.select(".offer_link").attr("href"),
+        price = offerElement.select(".current-price_value").html().substring(0, offerElement.select(".current-price_value").html().indexOf("&nbsp")).replaceAll("\\s", "").toDouble,
+        link = "https://www.itaka.pl" + offerElement.select(".offer_link").attr("href"),
         duration = offerElement.select(".offer_date span").not(".offer_date_icon-container").text().substring(16, 17).toInt,
         hotelRate = offerElement.select(".star").toArray().length - offerElement.select(".star_half").toArray().length * 0.5,
         reviewRate = replaceNullStringToZeroString(offerElement.select(".hotel-rank").text()).toDouble,
@@ -146,8 +146,8 @@ class TravelAgencyService @Inject() (){
     if (onlyLastMinute) sourceUrl = sourceUrl + "/last-minute"
     if (onlyAllInclusive) sourceUrl = sourceUrl + "/all-inclusive"
 
-    if (dateFrom!=null) sourceUrl = sourceUrl + "/t1," + format2.format(dateFrom)
-    if (dateTo!=null) sourceUrl = sourceUrl + "/t2," + format2.format(dateTo)
+    if (dateFrom != null) sourceUrl = sourceUrl + "/t1," + format2.format(dateFrom)
+    if (dateTo != null) sourceUrl = sourceUrl + "/t2," + format2.format(dateTo)
     sourceUrl = sourceUrl + "/adt," + numberOfPersons
     sourceUrl = sourceUrl + "/samolotem"
     sourceUrl = sourceUrl + "/s," + minHotelRate + "-5"
@@ -156,38 +156,39 @@ class TravelAgencyService @Inject() (){
 
     val htmlDocument = Jsoup.connect(sourceUrl).get()
     val offersDomElements = htmlDocument.select("div.s2o").asScala
-    val offersData = for(offerElement <- offersDomElements) yield Offer(
+    val offersData = for (offerElement <- offersDomElements) yield Offer(
       no = 1,
       name = offerElement.select(".s2o_hot a").text(),
       travelAgencyName = "Traveliada",
       price = offerElement.select(".s2o_mob1 .s2o_cena span").text().toDouble,
       link = offerElement.select(".s2o_hot a").attr("href"),
-      duration = offerElement.select(".s2o_mob1 .s2o_dni").textNodes().get(0).text().substring(0,1).toInt,
+      duration = offerElement.select(".s2o_mob1 .s2o_dni").textNodes().get(0).text().substring(0, 1).toInt,
       hotelRate = offerElement.select(""".s2o_star img[src="/themes/images/star_1.png"]""").size(),
-      reviewRate = BigDecimal(replaceNullStringToZeroString(offerElement.select(".s2o_ocena").text()).substring(0,3).toDouble*6/10).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble,
+      reviewRate = BigDecimal(replaceNullStringToZeroString(offerElement.select(".s2o_ocena").text()).substring(0, 3).toDouble * 6 / 10).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble,
       isAllInclusive = offerElement.select(".s2o_w").text().contains("all inclusive")
     )
     offersData.toList.filter(offer => offer.duration >= minDays).take(20)
   }
 
-  private def getTUIOffers(dateFrom: Date, dateTo: Date, countries: List[String], numberOfPersons: Int, minDays: Int, minHotelRate: Int,onlyLastMinute: Boolean, onlyAllInclusive: Boolean): List[Offer] = {
+  private def getTUIOffers(dateFrom: Date, dateTo: Date, countries: List[String], numberOfPersons: Int, minDays: Int, minHotelRate: Int, onlyLastMinute: Boolean, onlyAllInclusive: Boolean): List[Offer] = {
     val country = countries.head
     var countryShortcut = ""
-    if(!countryCodesTUI.contains(country.toUpperCase())) {
-      return null
+    if (!countryCodesTUI.contains(country.toUpperCase())) {
+      return List()
     } else {
       countryShortcut = countryCodesTUI(country.toUpperCase())
     }
 
-    var site =""
-    if(onlyLastMinute) site="last-minute"
+    var site = ""
+    if (onlyLastMinute) site = "last-minute"
 
     val dateFromFormatted = format3.format(dateFrom)
     val dateToFormatted = format3.format(dateTo)
-    var body = """{"offerType":"BY_PLANE","childrenBirthdays":[],"departureDateFrom":""""+dateFromFormatted+"""","departureDateTo":""""+dateToFormatted+"""","departuresCodes":[],"destinationsCodes":["""" + countryShortcut
-    body = body + """"],"numberOfAdults":""" + numberOfPersons+""","durationFrom":""""+minDays+"""","durationTo":"30","site":""""+site+"""","metaData":{"page":0,"pageSize":30,"sorting":"price"},"filters":[{"filterId":"priceSelector","selectedValues":[]},{"filterId":"board","selectedValues":[]},{"filterId":"amountRange","selectedValues":[""]},
+    var body = """{"offerType":"BY_PLANE","childrenBirthdays":[],"departureDateFrom":"""" + dateFromFormatted + """","departureDateTo":"""" + dateToFormatted + """","departuresCodes":[],"destinationsCodes":["""" + countryShortcut
+    body = body + """"],"numberOfAdults":""" + numberOfPersons + ""","durationFrom":"""" + minDays + """","durationTo":"30","site":"""" + site +
+      """","metaData":{"page":0,"pageSize":30,"sorting":"price"},"filters":[{"filterId":"priceSelector","selectedValues":[]},{"filterId":"board","selectedValues":[]},{"filterId":"amountRange","selectedValues":[""]},
                  {"filterId":"flight_category","selectedValues":[]},{"filterId":"minHotelCategory","selectedValues":[""""
-    body = body + minHotelRate+"""s"]},{"filterId":"tripAdvisorRating","selectedValues":["defaultTripAdvisorRating"]},{"filterId":"beach_distance","selectedValues":["defaultBeachDistance"]},{"filterId":"facilities","selectedValues":[]},{"filterId":"WIFI","selectedValues":[]}"""
+    body = body + minHotelRate + """s"]},{"filterId":"tripAdvisorRating","selectedValues":["defaultTripAdvisorRating"]},{"filterId":"beach_distance","selectedValues":["defaultBeachDistance"]},{"filterId":"facilities","selectedValues":[]},{"filterId":"WIFI","selectedValues":[]}"""
     if (onlyAllInclusive) body = body + """,{"filterId":"board","selectedValues":["GT06-AI GT06-FBP"]}"""
     body = body + """,{"filterId":"sport_and_wellness","selectedValues":[]},{"filterId":"room_type","selectedValues":[]},{"filterId":"additionalType","selectedValues":[]}]}"""
     val result = Http("https://www.tui.pl/search/offers")
@@ -195,34 +196,39 @@ class TravelAgencyService @Inject() (){
       .header("Content-Type", "application/json")
       .header("Charset", "UTF-8")
       .option(HttpOptions.readTimeout(1000000)).asString
+    if (result.body.isEmpty) return List()
     val json: JsValue = Json.parse(result.body)
     val trips = json \ "offers"
 
-    implicit val offerRead: Reads[ReadOffer] = (
-        (__  \ "hotelName").read[String] ~
-        (__ \ "discountPerPersonPrice").read[String].map(f => f.toDouble) ~
-        (__  \ "offerUrl").read[String] ~
-        (__ \ "duration").read[Int] ~
-        (__ \ "hotelStandard").readWithDefault(0.0) ~
-        (__ \ "tripAdvisorRating").readWithDefault(0.0)~
-        (__ \ "boardType").read[String]
-      )(ReadOffer)
-    implicit val offersRead: Reads[List[ReadOffer]] = Reads.list(offerRead)
-    val offers = trips.get.validate[List[ReadOffer]](offersRead)
+    try {
+      implicit val offerRead: Reads[ReadOffer] = (
+        (__ \ "hotelName").read[String] ~
+          (__ \ "discountPerPersonPrice").read[String].map(f => f.toDouble) ~
+          (__ \ "offerUrl").read[String] ~
+          (__ \ "duration").read[Int] ~
+          (__ \ "hotelStandard").readWithDefault(0.0) ~
+          (__ \ "tripAdvisorRating").readWithDefault(0.0) ~
+          (__ \ "boardType").read[String]
+        ) (ReadOffer)
+      implicit val offersRead: Reads[List[ReadOffer]] = Reads.list(offerRead)
+      val offers = trips.get.validate[List[ReadOffer]](offersRead)
 
+      val finalOffers = for (offer <- offers.get) yield Offer(
+        no = 0,
+        name = offer.name,
+        travelAgencyName = "TUI",
+        price = offer.price,
+        link = "https://www.tui.pl" + offer.link,
+        duration = offer.duration,
+        hotelRate = offer.hotelRate,
+        reviewRate = offer.reviewRate,
+        isAllInclusive = offer.foodStandard.toLowerCase.contains("all inclusive")
+      )
+      finalOffers.take(20)
+    } catch {
+      case e : Exception => return List()
+    }
 
-    val finalOffers = for (offer <- offers.get) yield Offer (
-      no = 0,
-      name = offer.name,
-      travelAgencyName = "TUI",
-      price = offer.price,
-      link = "https://www.tui.pl" + offer.link,
-      duration = offer.duration,
-      hotelRate = offer.hotelRate,
-      reviewRate = offer.reviewRate,
-      isAllInclusive = offer.foodStandard.toLowerCase.contains("all inclusive")
-    )
-    finalOffers.take(20)
   }
 
   private def replaceNullStringToZeroString(string: String): String = {
